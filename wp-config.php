ac0c6dfdf910b2310353939e915a58ede4d2b279
panwang12 <615766378@qq.com>
2024-09-22 15:14:10 +0800

    add plugin

--- wp-config.php
+++ wp-config.php
@@ -21,13 +21,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'suncorn_db' );
+define( 'DB_NAME', 'local' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '123456' );
+define( 'DB_PASSWORD', 'root' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
